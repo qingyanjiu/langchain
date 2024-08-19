e7c2d53a444864a9c92f4457600d46c223828189
LouisLiu <qingyanjiu@hotmail.com>
2024-08-19 08:30:52 +0800

    实现流式调用

--- l22/l22-1.py
+++ l22/l22-1.py
@@ -1,9 +1,13 @@
 import sys
 import asyncio
 
-sys.path.append('.')
-from spark_llm import gen_spark_llm
-llm = gen_spark_llm(streaming=True)
+# sys.path.append('.')
+# from spark_llm import gen_spark_llm
+# llm = gen_spark_llm(streaming=True)
+
+from langchain_community.llms.ollama import Ollama
+# 实例化一个大模型工具
+llm = Ollama(base_url='http://localhost:11434', model="llama3-cn", temperature=1, verbose=True)
 
 # 导入所需的库和模块
 from langchain.schema import HumanMessage, SystemMessage
@@ -13,10 +17,10 @@ class CommandlineChatbot:
     # 在初始化时，设置花卉行家的角色并初始化聊天模型
     def __init__(self):
         self.chat = llm
-        self.messages = [SystemMessage(content="你是一个跑团主持人 。")]
+        self.messages = [SystemMessage(content="你是一个程序员 。")]
 
     # 定义一个循环来持续与用户交互
-    def chat_loop(self):
+    async def chat_loop(self):
         print("Chatbot 已启动! 输入'exit'来退出程序。")
         while True:
             user_input = input("你: ")
@@ -27,13 +31,12 @@ class CommandlineChatbot:
             # 将用户的输入添加到消息列表中，并获取机器人的响应
             self.messages.append(HumanMessage(content=user_input))
             chunks = []
-            for chunk in self.chat.stream(self.messages):
+            async for chunk in self.chat.astream(self.messages):
                 chunks.append(chunk)
-                for c in chunk:
-                    print(c, end="", flush=True)
+                print(chunk, end="", flush=True)
             print('\n')
 
 # 如果直接运行这个脚本，启动聊天机器人
 if __name__ == "__main__":
     bot = CommandlineChatbot()
-    bot.chat_loop()
+    asyncio.run(bot.chat_loop())
