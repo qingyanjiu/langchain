57f54071bc12746bebfcd40f3e2f659f70c24125
LouisLiu <qingyanjiu@hotmail.com>
2025-04-15 08:44:32 +0800

    删除PPT项目

--- ppt-generator/main.py
+++
@@ -1,5 +0,0 @@
-from data_gen import slide_data_gen
-from ppt_gen import ppt_gen
-
-data = slide_data_gen('党课培训', page_num=5)
-ppt_file = ppt_gen(data)
--- ppt-generator/readme.md
+++
@@ -1,4 +0,0 @@
-### 依赖
-```shell
-pip3 install python-pptx  -i https://mirrors.aliyun.com/pypi/simple/
-```
